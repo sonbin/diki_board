828f50046b8b5f0824403221308b2b2eaee088fb
sonbin34 <sonbin34@gmail.com>
2017-02-13 21:46:00 +0900

    2017/2/13  Diki보드 수정,삭제 기능 제작

--- routes/contents.js
+++ routes/contents.js
@@ -5,7 +5,7 @@ var BoardContents = require('../model/dikie_schema');
 
 router.get('/',function(req,res){
 
-	BoardContents.find({}).sort({date:-1}).exec(function(err,rawDatas){
+	BoardContents.find({deleted:false}).sort({date:-1}).exec(function(err,rawDatas){
 		if(err) throw err;
 		res.render('board',{content:rawDatas});
 	});
@@ -13,13 +13,24 @@ router.get('/',function(req,res){
 
 router.post('/',function(req,res){
 
+	var mode = req.param('mode');
+
 	var addWriter = req.body.addWriter;
 	var addTitle = req.body.addTitle;
 	var addContents = req.body.addContent;
 	var addPassword = req.body.addPassword;
 
-	addboard(addTitle,addWriter,addPassword,addContents);
-	res.redirect('/');
+	var modTitle = req.body.modTitle;
+	var modContent = req.body.modContent;
+	var modID = req.body.modID;
+
+	if(mode=='add'){
+		addboard(addTitle,addWriter,addPassword,addContents);
+		res.redirect('/');
+	}else{
+		modboard(modID,modTitle,modContent);
+		res.redirect('/');
+	}
 });
 
 
@@ -37,10 +48,28 @@ router.get('/view',function(req,res){
 				if(err) throw err;
 				res.render('boardDetail',{content:rawContents});
 			})
-
 	})
-
 })
+
+
+router.get('/password',function(req,res){
+	var id=req.param('id');
+
+	BoardContents.findOne({_id:id},function(err,rawContents){
+		res.send(rawContents.password);
+	})
+})
+
+
+router.get('/delete',function(req,res){
+	var contentID = req.param('id');
+	BoardContents.update({_id:contentID},{$set:{deleted:true}},function(err){
+		if(err) throw err;
+		res.redirect('/');
+	})
+})
+
+
 
 
 function addboard(title,writer,password,content){
@@ -55,6 +84,20 @@ function addboard(title,writer,password,content){
 		newBoardContent.save(function(err){
 			if(err) throw err;
 		});
+}
+
+function modboard(id,title,content){
+	var modContent = content.replace(/\r\n/gi,"\\r\\n");
+	BoardContents.findOne({_id:id},function(err,originContent){
+		if(err) throw err;
+		originContent.updated.push({title:originContent.title,contents:originContent.contents});
+		originContent.save(function(err){
+			if(err) throw err;
+		});
+	});
+	BoardContents.update({_id:id},{$set:{title:title,contents:modContent,date:Date.now()}},function(err){
+		if(err) throw err;
+	});
 }
 
 module.exports = router;
